5fcf05f97e59722c853faadd5aeb32dc70251852
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:25:29 +0700

    PBOIF2-10119068-Latihan7-Integer

---
+++ src/pboif2/pkg10119068/latihan7/integer/PBOIF210119068Latihan7Integer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan7.integer;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Integer
+ */
+
+public class PBOIF210119068Latihan7Integer {
+    public static void main(String[] args){
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120 ;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short: " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+}
